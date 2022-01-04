ba815735abab15cf09bb25538d04da7377878816
xiaoliu <1327133225@qq.com>
2022-01-04 14:51:17 +0800

    auto

--- v2 copy 2/request.go
+++ v2 copy 2/request.go
@@ -4,23 +4,17 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"time"
 )
 
-type Result struct {
-	Response *http.Response // 原始返回信息
-	Body     string         // 返回的 Body 字符串
-	Duration time.Duration  // 执行耗时
-}
+func Request(url string, method string, data string, Headers map[string]string, parms map[string]string) (body []byte, res *http.Response, err error) {
 
-func Request(url string, method string, data string, Headers map[string]string, parms map[string]string) (Resulter Result, err error) {
-	startTime := time.Now() // 函数开始时间
 	payload := strings.NewReader(data)
 
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, payload)
 	if err != nil {
 		return
+		// log.Panicln(err)
 	}
 
 	// 解析参数
@@ -41,23 +35,18 @@ func Request(url string, method string, data string, Headers map[string]string, 
 	}
 
 	// 发送请求
-	res, err := client.Do(req)
+	res, err = client.Do(req)
 	if err != nil {
 		return
 	}
-
-	Resulter.Response = res
 
 	// 解析返回数据
-	body, err := ioutil.ReadAll(res.Body)
+	body, err = ioutil.ReadAll(res.Body)
 	if err != nil {
 		return
 	}
-
-	Resulter.Body = string(body)
 
 	defer res.Body.Close() // 延迟关闭
 
-	Resulter.Duration = time.Since(startTime)
 	return
 }
--- v2 copy 2/request_test.go
+++ v2 copy 2/request_test.go
@@ -2,6 +2,7 @@ package tools_test
 
 import (
 	"fmt"
+	"log"
 	"testing"
 
 	"github.com/xiaoliuxiao6/tools/v2"
@@ -15,22 +16,16 @@ func TestCookie(t *testing.T) {
 	headers := map[string]string{
 		"cookie": "_ga=GA1.2.1244375557.1639365528; lang=zh; MEIQIA_TRACK_ID=1ucBijMktnNZkNfDEUvhI2fuG69; auth_token=eyJldCI6MTY0MTQ0OTIzNiwiZ2lkIjo2NCwidWlkIjoyMjQ2MjI0LCJ2IjowfQ==.kndo3Lx4kcxdDbi4x7Y0u5wC4x1dLomM9Utz2y/K8PFZlLx87dGy26bJ0DtXipji/wTSEHsyTa5q9ZgA2KhbHrs7oLxHZ+deJiDo8OSgox+CYaEjff+0IjGCgYCLeJUw+GiH+xcNY1qaCdMNjYVf+AotMRNWhW6dyBwAzYI245AXBIO1zB8Pduq2DwCvwvf/Av/dW0ZhH2zfJOyZFzCTRaslDJMFEzspWHENZ85PSVGZHlPxnOHJFNDpeXRLMR2qM1Enxr8Z14TCauu0rUtOi7GA0WsQ8zs4ldlZNXO1Sv1NXMO3uSWbnUeY0HIT6EqVW0ipAFbr2jbZHLzuH4AcLg==; _gid=GA1.2.466490370.1640827957; MEIQIA_VISIT_ID=22z6EbC1k8505p6siaf4v0ivreZ",
 	}
-	result, err := tools.Request(url, method, data, headers, headers)
-
+	body, result, err := tools.Request(url, method, data, headers, headers)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
-	fmt.Println("耗时: ", result.Duration)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
+	fmt.Println("状态码: ", result.StatusCode)
+	fmt.Println(err)
+	fmt.Println(string(body))
+	fmt.Println(result.Header)
 
-	// fmt.Println("状态码: ", result.StatusCode)
-	// fmt.Println(err)
-	// fmt.Println(string(body))
-	// fmt.Println(result.Header)
-
-	// fmt.Println("------------------")
-	// fmt.Println(result.Request)
+	fmt.Println("------------------")
+	fmt.Println(result.Request)
 }
