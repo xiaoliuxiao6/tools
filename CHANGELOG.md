1ad1cc019afbf8cb3dfa7dd612b0a8a231b009c6
xiaoliu <1327133225@qq.com>
2022-01-10 11:59:55 +0800

    auto

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-# 1.0.29
+# 1.0.30
 # go get github.com/xiaoliuxiao6/tools@v1.0.2
 
---
+++ v2/prometheus_test.go
@@ -0,0 +1,26 @@
+package tools_test
+
+import (
+	"testing"
+
+	"github.com/xiaoliuxiao6/tools"
+)
+
+// 使用方法演示
+func TestUsege(t *testing.T) {
+	// 1.注册指标
+	metrics := tools.InitPrometheus("opensea", "gala")
+
+	// 2.添加指标
+	labels := map[string]string{
+		"type": "1",
+	}
+	metrics.AddGaugeVec("测试1", "帮助内容1", labels, 1)
+	metrics.AddGaugeVec("测试11", "帮助内容11", labels, 1)
+
+	// 3.（可选）打印指标
+	metrics.PrintMetrics()
+
+	// 3.（可选）将指标写入文件
+	metrics.WriteTextfile("")
+}
